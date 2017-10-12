5a7c9487694206fb95f1cc3c38dfae65205e8e7e
Niema Moshiri <niemamoshiri@gmail.com>
2017-10-12 00:30:09 -0700

    Updated figure to be ROC curve

--- figures/plot_accuracy.py
+++ figures/plot_accuracy.py
@@ -21,17 +21,16 @@ def read_data(f):
     data = {}
     for line in open(f).read().strip().splitlines()[1:]:
         parts = line.split(',')
-        rep = parts[0]; TP = float(parts[2]); FP = float(parts[4]); FN = float(parts[5])
+        rep,thresh = parts[0:2]; TP,TN,FP,FN = [float(e) for e in parts[2:]]
         if rep not in data:
-            data[rep] = {'Precision':[],'Recall':[]}
+            data[rep] = {'Precision':[],'Recall':[],'FPR':[]}
         if FP == 0:
             precision = 1
         else:
             precision = TP/(TP+FP)
         data[rep]['Precision'].append(precision)
-        data[rep]['Recall'].append(TP/(TP+FN))
-    for rep in data:
-        data[rep]['Recall'],data[rep]['Precision'] = zip(*sorted(zip(data[rep]['Recall'], data[rep]['Precision'])))
+        data[rep]['Recall'].append(TP/(TP+FN)) # same as True Positive Rate (TPR)
+        data[rep]['FPR'].append(FP/(FP+TN)) # False Positive Rate (FPR)
     return data
 
 # read data
@@ -45,22 +44,29 @@ axes.set_xlim([0,1])
 axes.set_ylim([0,1])
 x = []; y = []
 for rep in data_max:
-    x += data_max[rep]['Recall']
-    y += data_max[rep]['Precision']
+#    x += data_max[rep]['Recall']
+#    y += data_max[rep]['Precision']
+    x += data_max[rep]['FPR']
+    y += data_max[rep]['Recall']
 x,y = zip(*sorted(zip(x,y)))
 y_avg = movingaverage(y, 50)
 plt.plot(x,y_avg,color=pal['max'])
 x = []; y = []
 for rep in data_maxsubtree:
-    x += data_maxsubtree[rep]['Recall']
-    y += data_maxsubtree[rep]['Precision']
+#    x += data_maxsubtree[rep]['Recall']
+#    y += data_maxsubtree[rep]['Precision']
+    x += data_max[rep]['FPR']
+    y += data_max[rep]['Recall']
 x,y = zip(*sorted(zip(x,y)))
 y_avg = movingaverage(y, 50)
 plt.plot(x,y_avg,color=pal['maxsubtree'])
 legend = plt.legend(handles=handles,bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., frameon=True)
-sns.plt.xlabel('Recall')
-sns.plt.ylabel('Precision')
-sns.plt.title('Precision vs. Recall (8 Clusters)')
+#sns.plt.xlabel('Recall')
+#sns.plt.ylabel('Precision')
+#sns.plt.title('Precision vs. Recall (8 Clusters)')
+sns.plt.xlabel('False Positive Rate (FPR)')
+sns.plt.ylabel('True Positive Rate (TPR)')
+sns.plt.title('ROC Curve of ClusterPicker-II Methods')
 sns.plt.show()
 fig.savefig('precision_vs_recall.pdf', format='pdf', bbox_extra_artists=(legend,), bbox_inches='tight')
 plt.close()
